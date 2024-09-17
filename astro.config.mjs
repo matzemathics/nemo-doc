e99c8192993b16f473be18f4f6b103012a9d6137
Matthias Meissner <geige.matze@gmail.com>
2024-09-17 14:45:03 +0200

    add tailwind and nemo colour-scheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://knowsys.github.io/',
   base: '/nemo-doc',
   outDir: './dist/nemo-doc',
-
   integrations: [
     starlight({
       title: 'Nemo Rule Engine',
+      customCss: [
+        './src/tailwind.css'
+      ],
       social: {
-        github: 'https://github.com/knowsys/nemo',
+        github: 'https://github.com/knowsys/nemo'
       },
       sidebar: [
         {
           label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
+          items: [ // Each item here is one entry in the navigation menu.
             { label: 'Installing', slug: 'guides/installing' },
             { label: 'Command Line', slug: 'guides/cli' },
             { label: 'Rule Language', slug: 'guides/tour' },
             { label: 'Browser Integration', slug: 'guides/wasm' },
-            { label: 'Python API', slug: 'guides/python' },
-          ],
+            { label: 'Python API', slug: 'guides/python' }
+          ]
         },
         {
           label: 'Language Reference',
           // todo: reorder
-          autogenerate: { directory: 'reference' },
-        },
-      ],
+          autogenerate: { directory: 'reference' }
+        }
+      ]
     }),
-  ],
+    tailwind()
+  ]
 });
